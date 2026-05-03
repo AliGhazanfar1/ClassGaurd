92f9efcbf26573d89f8c8e701c5f2d2e75ce7aab
AliGhazanfar1 <alighazanfar27804@gmail.com>
2026-05-03 17:26:49 +0530

    adminRoutes.js

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -12,6 +12,7 @@ router.put('/session/:id/stop', adminController.stopSession);
 router.get('/session/active', adminController.getActiveSession);
 router.get('/session/:id/attendance', adminController.getAttendanceList);
 router.get('/session/:id/export', adminController.exportToExcel);
+router.get('/students', adminController.getAllStudents);
 router.get('/logs', adminController.getLogs);
 
 module.exports = router;
